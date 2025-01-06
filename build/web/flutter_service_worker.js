11d4fec9092f30ee98c566ba93df66c873b38853
Richoux369 <richouxricky29@gmail.com>
2025-01-06 18:26:11 +0900

    Update build/web with assets

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "9248cf1f9d03874b9917242d720d992e
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "c6af5cbdcf1576edb63f65cf85c8ca54",
+"flutter_bootstrap.js": "c02671f959e62ed63b5effb336860fb9",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
